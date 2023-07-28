5568663d4ac70dfcb2a269aefa8c6d9fd200910a
Angaye <angayesandrine1gmail.com>
2023-07-28 18:06:59 +0200

    ajout du fichier js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-// const url = await fetch('https://api.github.com/users/Angaye/repos')
-// const response = await url.json()
-// console.log(response[4].html_url)
 
 
-function afficherSites(repos){
-    const siteBooki =document.getElementById('Projet-Booki');
-    siteBooki.innerText =repos
 
-   
-   
-}
+const projet_list = document.getElementById('projet_list');
 
 
 fetch('https://api.github.com/users/Angaye/repos')
 .then((response) =>response.json())
-.then(repos =>console.log(repos[4].html_url))
+.then(repos=>
+   repos.forEach(repo => {
+    const li = document.createElement('li');
+    const lien = document.createElement('a');
+
+    lien.href = repo.html_url;
+    lien.innerText = "Projet : "+repo.name;
+
+
+    li.append(lien);
+    projet_list.append(li);
+   })
+    
+    
+    
+    
+    )
 
 // fetch('https://api.github/repos/Angaye/Projet-Booki/topics')
 // .then((reponse) =>reponse.json())
